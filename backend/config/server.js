aa2a111370cd391ac3e632f693fbb228a4e30611
FrancoAdrianCarrizo <franco.carrizo@bitlogic.io>
2021-07-25 23:05:02 -0300

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '33b1e8690dc9855d8179ef37644e5318'),
+    },
+  },
+});
