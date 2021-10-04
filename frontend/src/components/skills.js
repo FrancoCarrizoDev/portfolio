0bf5c800c380ba035d430f1bc886cf2b5b033fe1
FrancoAdrianCarrizo <franco.carrizo@bitlogic.io>
2021-10-04 01:09:32 -0300

    delete comments

--- frontend/src/components/skills.js
+++ frontend/src/components/skills.js
@@ -25,66 +25,6 @@ export const Skills = ({ skills }) => {
               <Markdown source={skill.description} escapeHtml={false} />
             </div>
           ))}
-          {/* <div className=" col-12 col-md-4 d-flex flex-column align-items-center p-5 gap-sm skills__format">
-            <span className="skills__top__icons">
-              <i class="fas fa-code"></i>
-            </span>
-            <h4>Front-end</h4>
-            <p className="text-center">
-              I value simple content structure, clean design patterns, and
-              thoughtful interactions.
-            </p>
-            <h6>Things I enjoy designing:</h6>
-            <p>UX, UI, Web, Mobile, Apps, Logos</p>
-            <h6>Design Tools:</h6>
-            <p>Balsamiq Mockups</p>
-            <p>Figma</p>
-            <p>Invision</p>
-            <p>Marvel</p>
-            <p>Pen & Paper</p>
-            <p>Sketch</p>
-            <p>Zeplin</p>
-          </div>
-          <div className="col-12 col-md-4 border-right border-left border-md-y col-4 d-flex flex-column align-items-center p-5 gap-sm">
-            <span className="skills__top__icons">
-              <i class="fas fa-code-branch"></i>
-            </span>
-            <h4>Back-end</h4>
-            <p className="text-center">
-              I value simple content structure, clean design patterns, and
-              thoughtful interactions.
-            </p>
-            <h6 className="mb-0">Things I enjoy designing:</h6>
-            <p>UX, UI, Web, Mobile, Apps, Logos</p>
-            <h6 className="mb-0">Mentor Stats:</h6>
-            <p>Balsamiq Mockups</p>
-            <p>Figma</p>
-            <p>Invision</p>
-            <p>Marvel</p>
-            <p>Pen & Paper</p>
-            <p>Sketch</p>
-            <p>Zeplin</p>
-          </div>
-          <div className=" col-12 col-md-4 d-flex flex-column align-items-center p-5 gap-sm">
-            <span className="skills__top__icons">
-              <i class="fas fa-database"></i>
-            </span>
-            <h4 className="">Database</h4>
-            <p className="text-center">
-              I value simple content structure, clean design patterns, and
-              thoughtful interactions.
-            </p>
-            <h6 className="mb-0">Things I enjoy designing:</h6>
-            <p>UX, UI, Web, Mobile, Apps, Logos</p>
-            <h6 className="mb-0">Dev Tools:</h6>
-            <p>Balsamiq Mockups</p>
-            <p>Figma</p>
-            <p>Invision</p>
-            <p>Marvel</p>
-            <p>Pen & Paper</p>
-            <p>Sketch</p>
-            <p>Zeplin</p>
-          </div> */}
         </div>
       </div>
     </div>
